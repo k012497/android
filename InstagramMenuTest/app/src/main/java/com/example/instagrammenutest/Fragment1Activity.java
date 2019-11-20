6d0255ec645f609a0ee7c28a7295eb9375cc0937
소지니 <k012497@gmail.com>
2019-11-20 23:08:29 +0900

    1120_ BottomNavigationView

---
+++ InstagramMenuTest/app/src/main/java/com/example/instagrammenutest/Fragment1Activity.java
@@ -0,0 +1,25 @@
+package com.example.instagrammenutest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.instagrammenutest.R;
+
+public class Fragment1Activity extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment1, null, false);
+
+        return view;
+    }
+
+}

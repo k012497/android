1557aa736bc8470f483648cbc4ac1d5300c7b36a
k012497 <k012497@gmail.com>
2019-12-06 16:03:04 +0900

    delete comment

--- notificationtest/app/src/main/java/com/example/notificationtest/AlarmReceiver.java
+++ notificationtest/app/src/main/java/com/example/notificationtest/AlarmReceiver.java
@@ -28,24 +28,6 @@ public class AlarmReceiver extends BroadcastReceiver {
             Log.d("리시버", "off 색휘");
             context.stopService(intent);
         }
-
-//        switch (intent.getAction()){
-//            case "on":
-//                Log.d("getAction", "on");
-//
-//
-//                // Oreo(26) 버전 이후부터는 Background 에서 실행을 금지하기 때문에 Foreground 에서 실행해야 함
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                    context.startForegroundService(sIntent);
-//                } else {
-//                    context.startService(sIntent);
-//                }
-//                break;
-//
-//            case "off":
-//                Log.d("getAction", "off");
-//                context.stopService(intent);
-//                break;
-//        }
+        
     }
 }
